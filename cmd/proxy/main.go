6cb453503954297e376d33ae66b0e82a8cb7bd84
tadeusz-cirocki <junesuprise@gmail.com>
2026-04-02 02:15:32 +0200

    add initial proxy server implementation

---
+++ cmd/proxy/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	target, err := url.Parse("http://localhost:3001")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Incoming request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+
+		proxy.ServeHTTP(w, r)
+	}
+
+	log.Println("Starting proxy on :8080")
+	err = http.ListenAndServe(":8080", http.HandlerFunc(handler))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
